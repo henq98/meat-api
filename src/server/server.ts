5da2a73113d6904f832d9d3dabfcb3d63e60100e
henriqueyang <henriqueyang17@gmail.com>
2019-10-22 23:40:18 -0300

    first commit

---
+++ src/server/server.ts
@@ -0,0 +1,35 @@
+import * as restify from 'restify'
+
+import { Router } from '../common/router'
+import { environment } from '../common/environment'
+
+export class Server {
+  application: restify.Server;
+
+  initRoutes (routers: Router[]): Promise<any> {
+    return new Promise((resolve, reject) => {
+      try {
+        this.application = restify.createServer({
+          name: 'meat-api',
+          version: '1.0.0'
+        })
+
+        this.application.use(restify.plugins.queryParser())
+
+        for (const router of routers) {
+          router.applyRoutes(this.application)
+        }
+
+        this.application.listen(environment.server.port, () => {
+          resolve(this.application)
+        })
+      } catch (err) {
+        reject(err)
+      }
+    })
+  }
+
+  bootstrap (routers: Router[] = []): Promise<Server> {
+    return this.initRoutes(routers).then(() => this)
+  }
+}
